5310d31bbad402d587813cd62e3fc99c01eaa5aa
spike <morndragon@126.com>
2016-04-08 10:23:09 +0800

    添加信息

---
+++ SearchPage.js
@@ -0,0 +1,20 @@
+/**
+ * Created by wangchenlong on 16/4/8.
+ */
+
+'use strict';
+
+var React = require('react-native');
+
+// 用于去掉React的前缀, 不用写React.Text, 直接写Text
+var {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+  ActivityIndicatorIOS,
+  Image,
+  Component,
+  } = React;
+
